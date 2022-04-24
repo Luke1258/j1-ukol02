e224edb9f5fd0773d012fd12cb2e174b68b4c8a2
Lukas Zradula <lukas.zradula@sap.com>
2022-04-24 18:54:53 +0200

    added color mishmash

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -7,8 +7,13 @@ import java.awt.*;
 public class HlavniProgram {
 
     Turtle zofka;
+
     public void main(String[] args) {
         zofka = new Turtle();
+
+        Color cervenaFarba;
+        cervenaFarba=new Color(255,0,0);
+
 //  Commented first part because of cleaning screen
 //        zofka.setLocation(300.0,500.0);
 //        nakresliRovnostrannyTrojuhelnik(100.0);
@@ -24,45 +29,46 @@ public class HlavniProgram {
         //Zmrzlina
         zofka.setLocation(250, 500);
         zofka.turnLeft(90);
-        nakresliRovnostrannyTrojuhelnik(200.0);
+        nakresliRovnostrannyTrojuhelnik(200.0, cervenaFarba);
         zofka.setLocation(225, 550);
         zofka.penUp();
         zofka.move(60);
         zofka.penDown();
-        nakresliKolecko(zofka, 30.0);
+        nakresliKolecko(zofka, 30.0, new Color(0,127,255));
 
         //Snehuliak
         zofka.setLocation(500, 670);
-        nakresliKolecko(zofka,35.0);
+        nakresliKolecko(zofka,35.0, cervenaFarba);
         zofka.setLocation(500, 400);
-        nakresliKolecko(zofka, 25.0);
+        nakresliKolecko(zofka, 25.0, new Color(0,127,255));
         zofka.setLocation(500, 210);
-        nakresliKolecko(zofka, 15.0);
+        nakresliKolecko(zofka, 15.0, cervenaFarba);
         zofka.setLocation(390, 300);
         zofka.turnLeft(90);
-        nakresliKolecko(zofka, 10.0);
+        nakresliKolecko(zofka, 10.0, new Color(0,127,255));
         zofka.turnRight(180);
         zofka.setLocation(580, 300);
-        nakresliKolecko(zofka, 10.0);
+        nakresliKolecko(zofka, 10.0, cervenaFarba);
 
         //Masinka
         zofka.setLocation(800, 650);
-        nakresliRovnostrannyTrojuhelnik(150.0);
+        nakresliRovnostrannyTrojuhelnik(150.0, cervenaFarba);
         zofka.move(50);
         nakresliObdlznik(zofka, 150.0, 300.0);
         zofka.turnRight(90);
         for (int i = 0; i < 2; i++) {
             zofka.move(100);
-            nakresliKolecko(zofka, 10.0);
+            nakresliKolecko(zofka, 10.0, new Color(0,127,255));
         }
         zofka.move(100);
         zofka.turnLeft(90);
         nakresliObdlznik(zofka, 300.0, 200.0);
-        nakresliKolecko(zofka, 26.5);
+        nakresliKolecko(zofka, 26.5, new Color(0,127,255));
 
     }
 
-    public void nakresliRovnostrannyTrojuhelnik(double velikostStrany) {
+    public void nakresliRovnostrannyTrojuhelnik(double velikostStrany, Color farbaCiary) {
+        zofka.setPenColor(farbaCiary);
         for (int i = 0; i < 3; i++) {
             zofka.move(velikostStrany);
             zofka.turnLeft(120.0);
@@ -82,7 +88,8 @@ public class HlavniProgram {
         }
     }
 
-    private void nakresliKolecko(Turtle turtle, double delkaStrany) {
+    private void nakresliKolecko(Turtle turtle, double delkaStrany, Color farbaCiary) {
+        zofka.setPenColor(farbaCiary);
         nakresliMnohouhelnik(turtle, delkaStrany, 24);
     }
 
